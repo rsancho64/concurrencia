9788c4191c60b8cbce6ea62c100e48becb5746a9
rsancho64 <rsancho@iesrioarba.es>
2024-11-12 11:05:49 +0100

    .

--- monolito.py
+++ monolito.py
@@ -1,8 +1,14 @@
 #! /usr/bin/python3
 
-def algo():
-    print("yo hago algo")
+def algo(veces):
+    print("algo: begin")
+    while veces:
+        print("algo!")
+        veces -= 1
+    print("algo: end")
+
+# - [ ] break vs continue vs return
 
 if __name__ == "__main__":
 
-    algo()
+    algo(5)
