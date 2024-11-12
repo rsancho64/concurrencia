68693bd9c6351a2725cde14c5b0de51925e5d827
rsancho64 <rsancho@iesrioarba.es>
2024-11-12 10:45:32 +0100

    first commit

---
+++ monolito.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+def algo():
+    print("yo hago algo")
+
+if __name__ == "__main__":
+
+    algo()
---
+++ user.monolito.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+import monolito
+
+if __name__ == "__main__":
+
+    monolito.main()
